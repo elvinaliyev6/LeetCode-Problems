3d33546cdae786f0a446856c703c45492fbeae6d
elvinaliyev6 <aliyevaliyevelvin@gmail.com>
2022-02-22 21:52:06 +0400

    solutions

--- 1.Two Sum/src/main/java/Main.java
+++ 1.Two Sum/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Salam");
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+
+        return null;
     }
 }
