9e3e71aece85cb44f7b102448d8b0295122daef9
elvinaliyev6 <aliyevaliyevelvin@gmail.com>
2022-03-26 14:17:03 +0400

    leetcode problems

---
+++ 1-50/14. Longest Common Prefix/src/com/company/main/Solution.java
@@ -0,0 +1,24 @@
+package com.company.main;
+
+import java.lang.*;
+
+public class Solution {
+
+    public String longestCommonPrefix(String[] S) {
+        if (S.length == 0) return "";
+        String prefix = S[0];
+        for (int i = 1; i < S.length; i++) {
+            while (S[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String args[]) {
+
+    }
+
+
+}
