b945f75b11363ad9e34839447b0ac3bed70ddda1
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-04-25 17:27:54 +0400

    solution of leetcode problems

---
+++ 201-250/217. Contains Duplicate/src/main/java/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ 201-250/219. Contains Duplicate II/src/main/java/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+
+                if (Math.abs(map.get(nums[i]) - i) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return false;
+    }
+
+
+}
