a81faa332b171c66a9cef5a88e2c0e92a006ecd5
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-05-10 14:29:37 +0400

    solution of leetcode problems in java

---
+++ 501-550/507. Perfect Number/src/main/java/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1)
+            return false;
+
+        int sum = 1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                sum += (i + num / i);
+        }
+
+        if (sum == num)
+            return true;
+
+        return false;
+    }
+}
